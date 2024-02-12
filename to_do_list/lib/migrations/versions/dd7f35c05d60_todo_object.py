6ba31f97a7a3c60e0c64c1966c3142f0e9b10fd5
Martin Nyaga <martinmugendi8@gmail.com>
2024-02-12 15:55:22 +0300

    resstructured folder

---
+++ to_do_list/lib/migrations/versions/dd7f35c05d60_todo_object.py
@@ -0,0 +1,24 @@
+"""Todo object
+
+Revision ID: dd7f35c05d60
+Revises: 
+Create Date: 2024-02-12 15:51:57.674173
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'dd7f35c05d60'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
